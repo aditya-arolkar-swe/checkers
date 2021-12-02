81bd77e8df48f3c899018c35168131fd6a87aa35
Aditya Arolkar <aditya.arolkar@berkeley.edu>
2021-12-02 06:02:54 -0800

    added a message to create a game with starport

--- vue/src/store/generated/alice/checkers/alice.checkers.checkers/module/index.d.ts
+++ vue/src/store/generated/alice/checkers/alice.checkers.checkers/module/index.d.ts
@@ -1,6 +1,7 @@
 import { StdFee } from "@cosmjs/launchpad";
 import { OfflineSigner, EncodeObject } from "@cosmjs/proto-signing";
 import { Api } from "./rest";
+import { MsgCreateGame } from "./types/checkers/tx";
 import { MsgCreatePost } from "./types/checkers/tx";
 export declare const MissingWalletError: Error;
 interface TxClientOptions {
@@ -12,6 +13,7 @@ interface SignAndBroadcastOptions {
 }
 declare const txClient: (wallet: OfflineSigner, { addr: addr }?: TxClientOptions) => Promise<{
     signAndBroadcast: (msgs: EncodeObject[], { fee, memo }?: SignAndBroadcastOptions) => Promise<import("@cosmjs/stargate").BroadcastTxResponse>;
+    msgCreateGame: (data: MsgCreateGame) => EncodeObject;
     msgCreatePost: (data: MsgCreatePost) => EncodeObject;
 }>;
 interface QueryClientOptions {
--- vue/src/store/generated/alice/checkers/alice.checkers.checkers/module/index.js
+++ vue/src/store/generated/alice/checkers/alice.checkers.checkers/module/index.js
@@ -2,8 +2,10 @@
 import { SigningStargateClient } from "@cosmjs/stargate";
 import { Registry } from "@cosmjs/proto-signing";
 import { Api } from "./rest";
+import { MsgCreateGame } from "./types/checkers/tx";
 import { MsgCreatePost } from "./types/checkers/tx";
 const types = [
+    ["/alice.checkers.checkers.MsgCreateGame", MsgCreateGame],
     ["/alice.checkers.checkers.MsgCreatePost", MsgCreatePost],
 ];
 export const MissingWalletError = new Error("wallet is required");
@@ -19,6 +21,7 @@ const txClient = async (wallet, { addr: addr } = { addr: "http://localhost:26657
     const { address } = (await wallet.getAccounts())[0];
     return {
         signAndBroadcast: (msgs, { fee, memo } = { fee: defaultFee, memo: "" }) => client.signAndBroadcast(address, msgs, fee, memo),
+        msgCreateGame: (data) => ({ typeUrl: "/alice.checkers.checkers.MsgCreateGame", value: data }),
         msgCreatePost: (data) => ({ typeUrl: "/alice.checkers.checkers.MsgCreatePost", value: data }),
     };
 };
